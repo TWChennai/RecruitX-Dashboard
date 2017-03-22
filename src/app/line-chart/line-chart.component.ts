68b485bbcadece48235f59eec24c76f4280db2f6
Saran <saranrajmalar@gmail.com>
2017-03-22 21:13:36 +0530

    [Saran/Arun] Styling on Line chart
    - give maring between interview stats circles

--- src/app/line-chart/line-chart.component.ts
+++ src/app/line-chart/line-chart.component.ts
@@ -5,10 +5,11 @@ import { Component, Input } from '@angular/core';
 })
 export class LineChartComponent {
 
+
     public lineChartData:Array<any> = [
-        {data: [65, 59, 80, 81, 56, 55, 40], label: 'IFaber'},
-        {data: [28, 48, 40, 19, 86, 27, 90], label: 'BCG'},
-        {data: [18, 48, 77, 9, 100, 27, 40], label: 'PAM'}
+        {data: [65, 59, 80, 81, 56, 55, 40], label: 'IFaber',lineTension: 0.2},
+        {data: [28, 48, 40, 19, 86, 27, 90], label: 'BCG',lineTension: 0.2},
+        {data: [18, 48, 77, 9, 100, 27, 40], label: 'PAM',lineTension: 0.2}
     ];
     public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
     public line_options:any = { responsive: true, scaleShowHorizontalLines: false,
@@ -17,24 +18,24 @@ export class LineChartComponent {
 
     public lineChartColors:Array<any> = [
         { // grey
-            backgroundColor: 'rgba(148,159,177,0.2)',
-            borderColor: 'rgba(148,159,177,1)',
+            backgroundColor: 'rgba(148,159,177,0)',
+            borderColor: '#2F8EF5',
             pointBackgroundColor: 'rgba(148,159,177,1)',
             pointBorderColor: '#fff',
             pointHoverBackgroundColor: '#fff',
             pointHoverBorderColor: 'rgba(148,159,177,0.8)'
         },
         { // dark grey
-            backgroundColor: 'rgba(77,83,96,0.2)',
-            borderColor: 'rgba(77,83,96,1)',
+            backgroundColor: 'rgba(77,83,96,0)',
+            borderColor: '#CC16C6',
             pointBackgroundColor: 'rgba(77,83,96,1)',
             pointBorderColor: '#fff',
             pointHoverBackgroundColor: '#fff',
             pointHoverBorderColor: 'rgba(77,83,96,1)'
         },
         { // grey
-            backgroundColor: 'rgba(148,159,177,0.2)',
-            borderColor: 'rgba(148,159,177,1)',
+            backgroundColor: 'rgba(148,159,177,0)',
+            borderColor: '#49A60C',
             pointBackgroundColor: 'rgba(148,159,177,1)',
             pointBorderColor: '#fff',
             pointHoverBackgroundColor: '#fff',
