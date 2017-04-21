c2674bb060146a7622bb601c5c319078065ddeed
arun <arunsa@thoughtworks.com>
2017-04-21 11:28:09 +0530

    extracted thanks page template to new html file

--- src/app/to-thank/to-thank.component.ts
+++ src/app/to-thank/to-thank.component.ts
@@ -2,13 +2,7 @@ import {Component, Input} from "@angular/core";
 import {Interview} from "../interview.model";
 @Component({
     selector: 'to-thank',
-    template: `<h1>RecruitX</h1>
-                <img src="../../../public/images/Star.png" style="width:350px; height:170px;"/>
-                <div class="title">You are our stars! Thank You for signing up :)</div>
-                <div class="panelists" *ngFor="let panelist of getPanelistsWithPartiallyFilledInterviews()">
-                    <i class="fa fa-user fa-3x" aria-hidden="true"></i>
-                    <div>{{panelist}}</div>
-                </div>`
+    templateUrl: 'to-thank.component.html'
 })
 
 export class ToThankComponent {
